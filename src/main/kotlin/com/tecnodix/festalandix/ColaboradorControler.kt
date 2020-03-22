bed7c2e44e75bdbb16ed17059061315996dbc40d
Cabomaldade <soutom@yahoo.com.br>
2020-03-22 01:49:07 -0300

    back e mustache configurado

--- src/main/kotlin/com/tecnodix/festalandix/ColaboradorControler.kt
+++ src/main/kotlin/com/tecnodix/festalandix/ColaboradorControler.kt
@@ -14,16 +14,16 @@ class ColaboradorControler(private val colaboradorRepository: ColaboradorReposit
     }
 
     @GetMapping("/todos")
-    fun all(): MutableIterable<Colaborador> = this.colaboradorRepository.findAll()
+    fun todos(): MutableIterable<Colaborador> = this.colaboradorRepository.findAll()
 
     @GetMapping("/{nome}")
-    fun byName(@PathVariable(value = "nome") nome: String): List<Colaborador> {
+    fun porNome(@PathVariable(value = "nome") nome: String): List<Colaborador> {
         val colaboradorPorNome = this.colaboradorRepository.findByNome(nome)
         return colaboradorPorNome
     }
 
     @PostMapping("/cadastrar")
-    fun checkIn(@RequestBody novoCadastro: Colaborador) {
+    fun cadastrar(@RequestBody novoCadastro: Colaborador) {
         val colaborador = Colaborador(novoCadastro._nome, novoCadastro._email, novoCadastro._senha)
         this.colaboradorRepository.save(colaborador)
         println("Log ColaboradorControler linha 22: Cadastro de novo colaborador efetuado")
